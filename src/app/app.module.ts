815e643d15c2fd4b404e6a295e6249e3c5b76b97
samiramahfud <samiramahfud24@gmail.com>
2018-10-02 18:26:29 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ServersComponent } from './servers/servers.component';
 import { TugasComponent } from './tugas/tugas.component';
 import { Tugas3Component } from './tugas3/tugas3.component';
 import { PegawaiComponent } from './pegawai/pegawai.component';
+import { ValidationComponent } from './validation/validation.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,10 @@ import { PegawaiComponent } from './pegawai/pegawai.component';
     ServersComponent,
     TugasComponent,
     Tugas3Component,
-    PegawaiComponent
+    PegawaiComponent,
+    ValidationComponent,
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
